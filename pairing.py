c8efe9c787a0917fc473ec1aa2afec0ee557ba3e
Prosto Chewey <prosto.chewey@gmail.com>
2014-07-09 12:03:17 +0400

    fix variable name

--- pairing.py
+++ pairing.py
@@ -7,7 +7,7 @@ import config
 
 __macos__ = sys.platform == 'darwin'
 
-if __macosx__:
+if __macos__:
     import pybonjour
 
     def register_callback(sdRef, flags, errorCode, name, regtype, domain):
@@ -16,8 +16,8 @@ if __macosx__:
             print '  name    =', name
             print '  regtype =', regtype
             print '  domain  =', domain
-    
-    
+
+
     data = {
         "DvNm" : config.remotename,
         "RemV" : "10000",
@@ -26,8 +26,8 @@ if __macosx__:
         "Pair" : name ,
         "txtvers" : "1"
        }
-    
-        
+
+
     def bonjoursocket(port):
         sdRef = pybonjour.DNSServiceRegister(name = name,
                                      regtype = regtype,
@@ -42,9 +42,9 @@ passcode = '1234'
 
 regtype = '_touch-remote._tcp'
 poll_interval = None
-    
 
-def serverConnexion(clientsocket): 
+
+def serverConnexion(clientsocket):
     rcv = clientsocket.recv(1024)
     """
     GET /pair?pairingcode=75D809650423A40091193AA4944D1FBD&servicename=985461928A772733 HTTP/1.1
@@ -56,7 +56,7 @@ def serverConnexion(clientsocket):
 
     ua = re.compile("User-Agent: (.*)")
     co = re.compile("GET.*?=([A-F0-9]*)&.*?=([A-F0-9]*)")
-    
+
     for l in rcv.split('\n'):
         m = ua.match(l)
         n = co.match(l)
@@ -67,26 +67,26 @@ def serverConnexion(clientsocket):
             pairingcode = n.group(1)
             servicename = n.group(2)
             print "Pairing code", pairingcode, "service name", servicename
-    
+
     expected = encode.itunes_pairingcode(passcode, name)
     print "Expected", encode.to_hex(expected)
-    
-    # any incoming requests are just pairing code related 
-    # return our guid regardless of input 
-    values = { 
-        'cmpg': '\x00\x00\x00\x00\x00\x00\x00\x01',
-        'cmnm': 'devicename', 
-        'cmty': 'ipod', } 
-    encoded = '' 
 
-    for key, value in values.iteritems(): 
-        encoded += '%s%s%s' % (key, struct.pack('>i', len(value)), value) 
-    header = 'cmpa%s' % (struct.pack('>i', len(encoded))) 
-    encoded = '%s%s' % (header, encoded) 
-    
+    # any incoming requests are just pairing code related
+    # return our guid regardless of input
+    values = {
+        'cmpg': '\x00\x00\x00\x00\x00\x00\x00\x01',
+        'cmnm': 'devicename',
+        'cmty': 'ipod', }
+    encoded = ''
+
+    for key, value in values.iteritems():
+        encoded += '%s%s%s' % (key, struct.pack('>i', len(value)), value)
+    header = 'cmpa%s' % (struct.pack('>i', len(encoded)))
+    encoded = '%s%s' % (header, encoded)
+
     "Fri, 31 Dec 1999 23:59:59 GMT"
     dt = datetime.datetime.today().strftime('%a, %d %b %Y %H:%M:%S %Z')
-    
+
     http_headers = """HTTP/1.0 200 OK
 Date: %s
 Content-Type: text/html
@@ -96,9 +96,9 @@ Content-Length: %s
 
     print http_headers, values
     clientsocket.send( http_headers )
-    clientsocket.send(encoded) 
+    clientsocket.send(encoded)
     clientsocket.close()
-    return 
+    return
 
 
 
@@ -110,29 +110,29 @@ def startserver(port):
 
 
 if __name__ == "__main__":
-    
+
 
     try:
         port    = config.defaultPort
         connected = False
         maxtries = 12
         tried = 0
-        
+
         while not connected and tried < maxtries:
             print "Creating server on port", port
             tried += 1
             try:
-                serversocket = startserver(port) 
+                serversocket = startserver(port)
                 readfrom = [serversocket]
                 connected = True
             except socket.error, e:
                 if e.errno == 48:
                     port += 1
-                    print "Address already in use, trying port", port 
+                    print "Address already in use, trying port", port
                 else:
                     print "Unknown error"
                     raise
-        """        
+        """
         if __macos__:
             print "Using pybonjour"
             sdRef = bonjoursocket( port )
@@ -143,21 +143,21 @@ if __name__ == "__main__":
             from ZeroconfService import ZeroconfService
             service = ZeroconfService(name=name, port=port, stype=regtype, text=avahi.dict_to_txt_array(data))
             service.publish()
-        """     
-            
-    
+        """
+
+
         while True:
             ready_to_read, w, in_error = select.select(readfrom, [], [], poll_interval)
-            
+
             if len(ready_to_read) > 0:
                 print "Finally something to do"
-                
+
                 if __macos__ and (sdRef in ready_to_read):
                     pybonjour.DNSServiceProcessResult(sdRef)
                 elif serversocket in ready_to_read:
                     (clientsocket, address) = serversocket.accept()
                     serverConnexion(clientsocket)
-        
+
                 else:
                     print ready_to_read
     finally:
@@ -165,5 +165,5 @@ if __name__ == "__main__":
             sdRef.close()
         else:
             service.unpublish()
-        serversocket.close() 
-    
+        serversocket.close()
+
